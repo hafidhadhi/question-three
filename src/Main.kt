6f4885ca2500fec8273556bb6feca8a4398cf726
hafidhadhi <hafidh.adhi@gmail.com>
2021-02-05 14:48:34 +0700

    initial commit

---
+++ src/Main.kt
@@ -0,0 +1,37 @@
+
+import org.json.JSONArray
+import org.json.JSONObject
+
+data class Inventory(
+    val id: Int?,
+    val name: String?,
+    val type: String?,
+    val tags: List<String>,
+    val purchasedAt: Long?,
+    val placement: Placement
+)
+
+data class Placement(
+    val id: Int?,
+    val name: String?
+)
+
+fun main(args: Array<String>) {
+    val data = JSONArray(JSON_DATA_STRING).map {
+        it as JSONObject
+        Inventory(
+            id = it.getInt("inventory_id"),
+            name = it.getString("name"),
+            type = it.getString("type"),
+            tags = it.getJSONArray("tags").map { tag -> tag as String },
+            purchasedAt = it.getLong("purchased_at"),
+            placement = Placement(
+                id = it.getJSONObject("placement").getInt("room_id"),
+                name = it.getJSONObject("placement").getString("name")
+            )
+        )
+    }
+    println(data)
+}
+
+val JSON_DATA_STRING = "[{\"inventory_id\":9382,\"name\":\"Brown Chair\",\"type\":\"furniture\",\"tags\":[\"chair\",\"furniture\",\"brown\"],\"purchased_at\":1579190471,\"placement\":{\"room_id\":3,\"name\":\"Meeting Room\"}},{\"inventory_id\":9380,\"name\":\"Big Desk\",\"type\":\"furniture\",\"tags\":[\"desk\",\"furniture\",\"brown\"],\"purchased_at\":1579190642,\"placement\":{\"room_id\":3,\"name\":\"Meeting Room\"}},{\"inventory_id\":2932,\"name\":\"LG Monitor 50 inch\",\"type\":\"electronic\",\"tags\":[\"monitor\"],\"purchased_at\":1579017842,\"placement\":{\"room_id\":3,\"name\":\"Lavender\"}},{\"inventory_id\":232,\"name\":\"Sharp Pendingin Ruangan 2PK\",\"type\":\"electronic\",\"tags\":[\"ac\"],\"purchased_at\":1578931442,\"placement\":{\"room_id\":5,\"name\":\"Dhanapala\"}},{\"inventory_id\":9382,\"name\":\"Alat Makan\",\"type\":\"tableware\",\"tags\":[\"spoon\",\"fork\",\"tableware\"],\"purchased_at\":1578672242,\"placement\":{\"room_id\":10,\"name\":\"Rajawali\"}}]"
